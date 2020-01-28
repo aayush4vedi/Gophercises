04dfcc177383ac9950d438013bb7d1dcd321d297
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-01-28 23:21:28 +0530

    Add flag for shuffle & timer

--- quiz/main.go
+++ quiz/main.go
@@ -36,8 +36,11 @@ func shuffled(questions []problem) []problem {
 
 func main() {
 	csvFile := flag.String("fileName", "problems.csv", "File containing questions in the format 'question, answer'")
+	timeLt := flag.Int("timer", 3, "Timer for test duration.Default value is 30 sec.")
 	shuffle := flag.Bool("shuffle", false, "Pass 'true' to shuffle the question order")
+
 	flag.Parse()
+
 	file, err := os.Open(*csvFile)
 	if err != nil {
 		fmt.Printf("Failed to open file: %v\n", *csvFile)
@@ -52,13 +55,26 @@ func main() {
 	if *shuffle == true {
 		questions = shuffled(questions)
 	}
+	timer1 := time.NewTimer(time.Duration(*timeLt) * time.Second)
+	fmt.Println("Press 'Enter' to start test.")
+
 	correct := 0
 	var inputAns string
 	for i, q := range questions {
 		fmt.Printf("Question #%v : %v ?\n", i, q.q)
-		fmt.Scanf("%s", &inputAns)
-		if inputAns == q.a {
-			correct++
+		ch := make(chan string)
+		go func() {
+			fmt.Scanf("%s", &inputAns)
+			ch <- inputAns
+		}()
+		select {
+		case <-timer1.C:
+			fmt.Printf("Score: %v / %v\n", correct, len(records))
+			return
+		case <-ch:
+			if inputAns == q.a {
+				correct++
+			}
 		}
 	}
 	fmt.Printf("Score: %v / %v\n", correct, len(records))
