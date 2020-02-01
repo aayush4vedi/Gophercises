aa841cf45aa4bf4323fea9710266c0d4265a2809
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-02 03:53:49 +0530

    @sitemap: Refactor for Parsing and Building links

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/Gophercises/link"
 )
@@ -20,29 +22,46 @@ import (
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "link of website you want to parse for")
 	flag.Parse()
+	pages := getPages(*urlFlag)
+	for _, p := range pages {
+		fmt.Println(p)
+	}
+}
 
-	resp, err := http.Get(*urlFlag)
+func getPages(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 	// io.Copy(os.Stdout, resp.Body)
-	links, _ := link.Parse(resp.Body)
-	for _, l := range links {
-		fmt.Println(l)
-	}
-	/*
-		Possible links:
-		A. partial links: `/path`
-		B. full links: starting with `http` or `https`
-		C. Anything else-not to be included in the list: fragments-`#signup`, `mailto@xyz.com`
-	*/
-	//for A.
 	reqUrl := resp.Request.URL
 	baseUrl := &url.URL{
 		Scheme: reqUrl.Scheme,
 		Host:   reqUrl.Host,
 	}
 	base := baseUrl.String()
-	fmt.Printf("%+v\n", base)
+	return getHrefs(resp.Body, base)
+}
+
+func getHrefs(r io.Reader, base string) []string {
+	var hrefs []string
+	links, _ := link.Parse(r)
+	/*
+		Possible links:
+		A. partial links: `/path`
+		B. full links: starting with `http` or `https`
+		C. Anything else-not to be included in the list: fragments-`#signup`, `mailto@xyz.com`
+	*/
+	for _, l := range links {
+		switch {
+		//for A.
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		//for B.
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	return hrefs
 }
