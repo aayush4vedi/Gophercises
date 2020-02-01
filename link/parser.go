dcbe12a164ba880a15b0c6bd09e0e000c171f458
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-02 01:07:28 +0530

    link: Add link parsing & clean-up on dfs

--- link/parser.go
+++ link/parser.go
@@ -1,10 +1,11 @@
 package link
 
-import "io"
+import (
+	"io"
+	"strings"
 
-import "golang.org/x/net/html"
-
-import "fmt"
+	"golang.org/x/net/html"
+)
 
 type Link struct {
 	Href string
@@ -16,17 +17,49 @@ func Parse(r io.Reader) ([]Link, error) {
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
-	return nil, nil
+	nodes := dfs(doc)
+	var links []Link
+	for _, n := range nodes {
+		links = append(links, buildLink(n))
+	}
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
+func dfs(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
 	}
-	fmt.Println(padding, msg)
+	var nodes []*html.Node
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+" ")
+		nodes = append(nodes, dfs(c)...)
 	}
+	return nodes
+}
+
+//get only the useful information from each node
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+	// return ret  //gives ugly string(with /n & multiple consecutive spaces)
+	return strings.Join(strings.Fields(ret), " ")
 }
