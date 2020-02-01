bcc3c2c5540ed06e2dd1c40e75307cd5ab96137d
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-02 00:30:05 +0530

    link: Add dfs

--- link/parser.go
+++ link/parser.go
@@ -2,11 +2,31 @@ package link
 
 import "io"
 
+import "golang.org/x/net/html"
+
+import "fmt"
+
 type Link struct {
 	Href string
 	Text string
 }
 
-func Parse(io.Reader) ([]Link, error) {
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+" ")
+	}
 }
