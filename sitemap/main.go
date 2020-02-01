84ebd93fb564a28516740a210e81559d79f4c780
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-02 03:07:16 +0530

    @sitemap: Add basic page parsing

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+
+	"github.com/Gophercises/link"
 )
 
-func main() {
-	urlFlag := flag.String("url", "https://www.google.com", "link of website you want to parse for")
-	flag.Parse()
-
-	fmt.Println(*urlFlag)
-
-	/*
+/*
 	1. GET the webpage
 	2. parse all the links on the page
 	3. build proper urls with our links
 	4. filter out any links w/ a diff domain
 	5. Find all pages (BFS)
 	6. print out XML
-	*/
+*/
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "link of website you want to parse for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// io.Copy(os.Stdout, resp.Body)
+	links, _ := link.Parse(resp.Body)
+	for _, l := range links {
+		fmt.Println(l)
+	}
+
 }
