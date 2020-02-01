9cdfba3034d32906ee17187665c0eef34f6f4475
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-02 04:21:26 +0530

    @sitemap: Add Filtering & BFS

--- sitemap/main.go
+++ sitemap/main.go
@@ -21,17 +21,46 @@ import (
 */
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "link of website you want to parse for")
+	maxDepth := flag.Int("depth", 10, "the maximum number of links deep to traverse")
 	flag.Parse()
-	pages := getPages(*urlFlag)
+	pages := bfs(*urlFlag, *maxDepth)
 	for _, p := range pages {
 		fmt.Println(p)
 	}
 }
 
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{}) //could have used `map[string]bool` here but empty structs use less memory.TODO: READ: https://dave.cheney.net/2014/03/25/the-empty-struct
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok { //TODO: add in notes
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range getPages(url) {
+				nq[link] = struct{}{} // first `{}` is type & second `{}` is instantiating it
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
 func getPages(urlStr string) []string {
 	resp, err := http.Get(urlStr)
 	if err != nil {
-		panic(err)
+		return []string{}
 	}
 	defer resp.Body.Close()
 	// io.Copy(os.Stdout, resp.Body)
@@ -63,5 +92,22 @@ func getHrefs(r io.Reader, base string) []string {
 			hrefs = append(hrefs, l.Href)
 		}
 	}
-	return hrefs
+	return filter(hrefs, withPrefix(base))
+}
+
+// 4. filter out any links w/ a diff domain
+func filter(links []string, keepFn func(string) bool) []string {
+	var filteredLinks []string
+	for _, l := range links {
+		if keepFn(l) {
+			filteredLinks = append(filteredLinks, l)
+		}
+	}
+	return filteredLinks
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
